98a8be5bacfdac846982c84ed75bcc7a0c1eedbb
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-04-05 10:35:17 -0600

    update contributors

--- src/app/about-page/contributors/contributors.ts
+++ src/app/about-page/contributors/contributors.ts
@@ -4,39 +4,39 @@ export const CONTRIBUTORS = {
       name: "Matias Niemelä",
       style: 'matsko',
       title: "Master Wizard - Angular Animation",
-      avatar: "https://avatars3.githubusercontent.com/u/93018?v=3&s=250",
+      avatar: "https://avatars3.githubusercontent.com/u/93018?v=3&s=200",
       githubURL: "https://github.com/matsko",
     },
     {
       name: "Miško Hevery",
       title: "(Grand)Father of Angular",
-      avatar: "https://avatars1.githubusercontent.com/u/111951?v=3&s=200",
+      avatar: "https://avatars1.githubusercontent.com/u/111951?v=3&s=150",
       githubURL: "https://github.com/mhevery",
     },
     {
       name: "Thomas Burleson",
       title: "Foolish Apprentice",
-      avatar: "https://avatars3.githubusercontent.com/u/210413?v=3&s=200",
+      avatar: "https://avatars3.githubusercontent.com/u/210413?v=3&s=150",
       githubURL: "https://github.com/thomasburleson",
     },
   ],
   secondary: [
     {
       name: "Ward Bell",
-      title: "DocFather of Angular",
-      avatar: "https://avatars2.githubusercontent.com/u/129061?v=3&s=200",
+      title: "Doc-Father of Angular",
+      avatar: "https://avatars2.githubusercontent.com/u/129061?v=3&s=150",
       githubURL: "https://github.com/wardbell"
     },
     {
       name: "Robert Messerlee",
       title: "Developer Extraordinaire",
-      avatar: "https://avatars1.githubusercontent.com/u/571363?v=3&s=200",
+      avatar: "https://avatars1.githubusercontent.com/u/571363?v=3&s=150",
       githubURL: "https://github.com/robertmesserle",
     },
     {
       name: "Elad Bezalel",
-      title: "Eager Fan",
-      avatar: "https://avatars0.githubusercontent.com/u/6004537?v=3&s=200",
+      title: "Developer Magician",
+      avatar: "https://avatars0.githubusercontent.com/u/6004537?v=3&s=150",
       githubURL: "https://github.com/EladBezalel",
     }
   ]
